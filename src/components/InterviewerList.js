fad0873f07be95be4752cb37b3844ea5714a15b4
SaranyaSagi <saranya108sagi@gmail.com>
2022-01-21 14:30:51 -0500

    create componenet interviewer list with map through array and incorporated props and unique keys

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import InterviewerListItem from "components/InterviewerListItem.js";
+
+import "components/InterviewerList.scss";
+
+export default function InterviewerList (props) {
+  const { interviewers, interviewer, setInterviewer } = props;
+
+  const parsedInterviewerListItem = interviewers.map((singleInterview) => {
+    return (
+      <InterviewerListItem
+        key={singleInterview.id}
+        id={singleInterview.id}
+        name={singleInterview.name}
+        avatar={singleInterview.avatar}
+        selected={singleInterview.id === interviewer}
+        setInterviewer={setInterviewer}
+      />
+    );
+  })
+
+  return(
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{parsedInterviewerListItem}</ul>
+    </section>
+  )
+}
