f47eea300efab3fb0d7d56f6632f38defd9af364
SaranyaSagi <saranya108sagi@gmail.com>
2022-01-22 07:05:15 -0500

    import header, show and empty component and pass props into show that will display only when props.interview exists

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,24 @@
-import React from "react";
-
+import React, { Fragment } from "react";
 //import classNames from "classnames";
+
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
   return (
-    <article className="appointment">{props.time ? `Appointment at ${props.time}` : "No Appointments" } </article>
+    <article className="appointment">
+      <Header time={props.time}/>
+      {props.interview ? 
+      <Show 
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+      /> 
+      : <Empty /> }
+    </article>
   );
 }
+
+//{props.time ? `Appointment at ${props.time}` : "No Appointments" }
