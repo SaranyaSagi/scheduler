4462b74ad95730452b4622b7f966717cadce36f3
SaranyaSagi <saranya108sagi@gmail.com>
2022-01-22 20:30:23 -0500

    fix bug where on save function for edit and create did not display the name and id in the array and instead just said [class], now it says ['name', id]

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -38,7 +38,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={event => cancel()}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button confirm onClick={event => props.onSave(student, interviewer)}>Save</Button>
         </section>
       </section>
     </main>
