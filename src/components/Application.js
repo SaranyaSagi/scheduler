c551c4c1d7181d0154b6d956573f8c88225d27d1
SaranyaSagi <saranya108sagi@gmail.com>
2022-01-22 20:32:36 -0500

    remove hard code data for days, add useState for days and set days and made use effect with axios request

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 import "components/Application.scss";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 const appointments = [
   {
@@ -63,6 +64,17 @@ const appointments = [
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    const URL = '/api/days'
+    axios
+      .get(URL)
+      .then(response => {
+        setDays([...response.data])
+        console.log(response.data)
+      })
+  }, [])
 
   const appointment = appointments.map((appointment) => {
     return (
