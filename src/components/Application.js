671a2b911ebd2a878d38b75653faf3a554622f81
SaranyaSagi <saranya108sagi@gmail.com>
2022-01-22 22:02:15 -0500

    combine states, made changes to useEffect, add prev to state set Days as function

--- src/components/Application.js
+++ src/components/Application.js
@@ -45,17 +45,24 @@ const appointments = [
 
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  // const [day, setDay] = useState("Monday");
+  // const [days, setDays] = useState([]);
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // you may put the line below, but will have to remove/comment hardcoded appointments variable
+    // appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
+  
 
   useEffect(() => {
-    const URL = '/api/days'
     axios
-      .get(URL)
-      .then(response => {
-        setDays([...response.data])
-        console.log(response.data)
-      })
+      .get('/api/days')
+      .then(response => setDays(response.data))
   }, [])
 
   const appointment = appointments.map((appointment) => {
@@ -78,8 +85,8 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
         <DayList 
-          days={days}
-          value={day}
+          days={state.days}
+          value={state.day}
           onChange={setDay}
         />
       </nav>
