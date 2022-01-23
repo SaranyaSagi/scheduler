23ea1e7aba18cf8e062854859c7912f6b006d4bf
SaranyaSagi <saranya108sagi@gmail.com>
2022-01-22 22:05:00 -0500

    removed old commented out function that is incorrect

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,15 +1,10 @@
-function selectUserByName(state, name) {
-  const filteredNames = state.users.filter(user => user.name === name);
-  return filteredNames;
-}
-
-// export function getAppointmentsForDay(state, day) {
-//   const filteredAppointments = state.days.map(daysObj => daysObj.day === day);
-//   return filteredAppointments;
+// function selectUserByName(state, name) {
+//   const filteredNames = state.users.filter(user => user.name === name);
+//   return filteredNames;
 // }
 
+// Same function as line 17 with forEach
 // export function getAppointmentsForDay(state, day) {
-
 //   let appointmentsArr = [];
 //   state.days.map(daysObj => {
 //     if (daysObj.name === day) {
