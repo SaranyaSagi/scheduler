a3a68eed7a410dcb1701fe80be59b792130f6fa0
SaranyaSagi <saranya108sagi@gmail.com>
2022-01-23 17:29:34 -0500

    import useVisualMode, add condition where SHOW appears if props.interview exits and if not EMPTY appears, Add modes, add transition(CREATE) onAdd and back() if onCanel

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,24 +1,38 @@
 import React, { Fragment } from "react";
-//import classNames from "classnames";
 
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+
+import useVisualMode from "hooks/useVisualMode";
 
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+  const interviewers = []
   return (
     <article className="appointment">
       <Header time={props.time}/>
-      {props.interview ? 
-      <Show 
-        student={props.interview.student}
-        interviewer={props.interview.interviewer}
-      /> 
-      : <Empty /> }
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (<Form interviewers={interviewers} onCancel={() => back()}/>)}
     </article>
-  );
+  ); 
 }
 
 //{props.time ? `Appointment at ${props.time}` : "No Appointments" }
