68a559358da9af502c750e9272ada5c5caeb5d66
SaranyaSagi <saranya108sagi@gmail.com>
2022-01-26 01:50:11 -0500

    removed some comments

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -3,28 +3,21 @@ describe("Navigation", () => {
     cy.visit("/");
   });
 
-  it("should navigate to Tuesday", () => {
-    cy.visit("/")
-    cy.contains("li", "Tuesday")
-      .click()
-      .should("have.css", "background-color", "rgb(242, 242, 242)");
-  })
-
-  //This doesnt work, says [data-testid=day] should exist but it doesnt
   // it("should navigate to Tuesday", () => {
   //   cy.visit("/")
-    
-  //   cy.contains("[data-testid=day]", "Tuesday")
-  //     .click()
-  //     .should("have.class", "day-list__item--selected")
-  // })
-
-  //This below doesnt work
-  // it("should navigate to Tuesday", () => {
-  //   cy.visit("/")
+  //   //cy.get("li").contains("Tuesday").click();
   //   cy.contains("li", "Tuesday")
   //     .click()
   //     .should("have.css", "background-color", "rgb(242, 242, 242)");
   // })
+
+  //Had to add data-testid="day" to day list
+  it("should navigate to Tuesday", () => {
+    cy.visit("/")
+    
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected")
+  })
 
 });
