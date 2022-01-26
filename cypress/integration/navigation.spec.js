012adefaf4ef63040990f93ecf9a0a30d3383809
SaranyaSagi <saranya108sagi@gmail.com>
2022-01-26 01:37:54 -0500

    add another test, uses visit, click, contains, should, have etc

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,4 +2,29 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/")
+    cy.contains("li", "Tuesday")
+      .click()
+      .should("have.css", "background-color", "rgb(242, 242, 242)");
+  })
+
+  //This doesnt work, says [data-testid=day] should exist but it doesnt
+  // it("should navigate to Tuesday", () => {
+  //   cy.visit("/")
+    
+  //   cy.contains("[data-testid=day]", "Tuesday")
+  //     .click()
+  //     .should("have.class", "day-list__item--selected")
+  // })
+
+  //This below doesnt work
+  // it("should navigate to Tuesday", () => {
+  //   cy.visit("/")
+  //   cy.contains("li", "Tuesday")
+  //     .click()
+  //     .should("have.css", "background-color", "rgb(242, 242, 242)");
+  // })
+
 });
