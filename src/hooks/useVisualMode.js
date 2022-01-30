a52793c363f75cdeae09cfb211662aa177a70436
SaranyaSagi <saranya108sagi@gmail.com>
2022-01-30 01:03:21 -0500

    refactor code to use prev, and protect against stale state and make error_save work

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,34 +1,67 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function useVisualMode(initial) {
- const [mode, setMode] = useState(initial);
- const [history, setHistory] = useState([initial]);
- 
- //making sure to create copies through spread and new variables so not to alter original data. 
- function transition(newMode, replace = false) {
-    if (!replace) {
-      setMode(newMode)
-      const newHistory = [...history]
-      newHistory.push(newMode)
-      setHistory(newHistory)
-      return;
-    } 
-    //break if replace is false
-  setMode(newMode)
-  const newHistory = [...history]
-  newHistory[newHistory.length-1] = newMode
-  setHistory(newHistory)
- }
+	const [history, setHistory] = useState([initial]);
 
- //Pops last item and sets mode to last item
- function back() {
-  if (history.length === 1) return;
-  const updatedHistory = [...history]
-  updatedHistory.pop()
-  setHistory(updatedHistory)
-  setMode(updatedHistory.slice(-1)[0])
- }
+	const transition = (newMode, replace = false) => {
+		setHistory((prev) => {
+			const newHistory = [...prev];
+			if (replace) {
+				newHistory.pop();
+			}
 
- return { mode, transition, back };
+			newHistory.push(newMode);
+			return newHistory;
+		});
+	};
+
+	const back = () => {
+		setHistory((prev) => {
+			const newHistory = [...prev];
+
+			if (newHistory.length < 2) {
+				return prev;
+			}
+
+			newHistory.pop();
+
+			return newHistory;
+		});
+	};
+
+	return { mode: history[history.length - 1], transition, back };
 }
 
+// import { useState } from "react";
+
+// export default function useVisualMode(initial) {
+//  const [mode, setMode] = useState(initial);
+//  const [history, setHistory] = useState([initial]);
+ 
+//  //making sure to create copies through spread and new variables so not to alter original data. 
+//  function transition(newMode, replace = false) {
+//     if (!replace) {
+//       setMode(newMode)
+//       const newHistory = [...history]
+//       newHistory.push(newMode)
+//       setHistory(newHistory)
+//       return;
+//     } 
+//     //break if replace is false
+//   setMode(newMode)
+//   const newHistory = [...history]
+//   newHistory[newHistory.length-1] = newMode
+//   setHistory(newHistory)
+//  }
+
+//  //Pops last item and sets mode to last item
+//  function back() {
+//   if (history.length === 1) return;
+//   const updatedHistory = [...history]
+//   updatedHistory.pop()
+//   setHistory(updatedHistory)
+//   setMode(updatedHistory.slice(-1)[0])
+//  }
+
+//  return { mode, transition, back };
+// }
