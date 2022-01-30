fa36ff1e8bb97953acddb8f2dee0cc7870839a01
SaranyaSagi <saranya108sagi@gmail.com>
2022-01-30 01:02:35 -0500

    refactor code for getAppts to remove lint error

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,31 +1,25 @@
 //In this function mapping through days object and returning an array for appointment id.
 export function getAppointmentsForDay(state, day) {
+  //found can be used to first isolate all the data where day = name
+  const found = state.days.find(d => day === d.name);
 
-  let appointmentsArr = [];
-  state.days.map(daysObj => {
-    if (daysObj.name === day) {
-     appointmentsArr = daysObj.appointments.map(apptId => state.appointments[apptId])
-    }
-  })
-  return appointmentsArr;
+  // need this for 6th hidden undefined slot and doesnt replace the whole array with undefined. 
+  if (state.days.length === 0 || found === undefined) return [];
+
+  //retirn just the appointments for that day
+  return found.appointments.map(id => state.appointments[id]);
 }
 
 //In this function mapping through days object and returning an array for interviewers.
 export function getInterviewersForDay(state, day) {
-
+  //found can be used to first isolate all the data where day = name
   const found = state.days.find(d => day === d.name);
 
   // This helps with dealing the 6th hidden undefined slot and doesnt replace the whole array with undefined. 
   if (state.days.length === 0 || found === undefined) return [];
 
+  //return just the interviewers for that day
   return found.interviewers.map(id => state.interviewers[id]);
-
-  //This original below function is returning undefined for Wed, Thurs, Fri
-  // let interviewersArr = [];
-  // state.days.map(daysObj => {if (daysObj.name === day) {
-  //     interviewersArr = daysObj.appointments.map(interviewData => state.interviewers[interviewData])}
-  // })
-  // return interviewersArr;
 }
 
 //returning an object that contains the interview data when passed an obj that contains interviewer
@@ -45,3 +39,20 @@ export function getInterview(state, interview) {
 
   return result;
 }
+
+///Old appointments selector
+ // let appointmentsArr = [];
+  // state.days.map(daysObj => {
+  //   if (daysObj.name === day) {
+  //    appointmentsArr = daysObj.appointments.map(apptId => state.appointments[apptId])
+  //   }
+  // })
+  // return appointmentsArr;
+
+//Old interviewers selector
+//This original below function is returning undefined for Wed, Thurs, Fri
+  // let interviewersArr = [];
+  // state.days.map(daysObj => {if (daysObj.name === day) {
+  //     interviewersArr = daysObj.appointments.map(interviewData => state.interviewers[interviewData])}
+  // })
+  // return interviewersArr;
